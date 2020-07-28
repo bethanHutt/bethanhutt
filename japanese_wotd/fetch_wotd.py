dd789a56a774f2e81d7cb9d29555ddccb27fd87a
bethanhutt <bethan157@hotmail.com>
2020-07-28 18:51:13 +0100

    wip

--- japanese_wotd/fetch_wotd.py
+++ japanese_wotd/fetch_wotd.py
@@ -1,9 +1,22 @@
+import os
 import feedparser
 
 URL = 'http://feeds.feedblitz.com/japanese-word-of-the-day'
+ROOT = os.path.dirname(os.path.dirname(__file__))
 
-rss_feed = feedparser.parse(URL)
-first_entry = rss_feed.entries[0]
 
-word_of_the_day = first_entry['title']
-print(word_of_the_day)
+def fetch_word_of_the_day():
+    rss_feed = feedparser.parse(URL)
+    first_entry = rss_feed.entries[0]
+
+    word_of_the_day = first_entry['title']
+    return word_of_the_day
+
+def main():
+    readme = ROOT + 'README.md'
+    print(readme)
+    word_of_the_day = fetch_word_of_the_day()
+    print(word_of_the_day)
+
+if __name__ == '__main__':
+    main()
