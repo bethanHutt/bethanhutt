d67e95233bdb1e985d0fa37e057500749fdb8fff
bethanhutt <bethan157@hotmail.com>
2020-08-03 17:27:55 +0100

    fetch_wotd script working

--- japanese_wotd/fetch_wotd.py
+++ japanese_wotd/fetch_wotd.py
@@ -1,15 +1,48 @@
 import os
+import re
 import feedparser
 
 URL = 'http://feeds.feedblitz.com/japanese-word-of-the-day'
 ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-WORD_TAG = '💬 Japanese Word of the Day: '
+WORD_FRAMEWORK = '💬 Japanese Word of the Day --> {}.'
+WOTD_TAG = 'japanese_wotd'
 
 
 class ReadMe():
-    def __init__(self, readme_data):
-        self.readme_data = readme_data
+    def __init__(self):
+        self.readme_data = self.get_readme_data()
+        self.new_readme = None
+
+
+    @property
+    def filepath(self):
+        filepath = os.path.join(ROOT, 'README.md')
+        return filepath
+    
+
+    def get_readme_data(self):
+        with open(self.filepath) as read_file:
+            readme_data = read_file.read()
+
+        return readme_data
+
+
+    def replace_word_of_the_day(self, new_wotd):
+        regex_pattern = re.compile(r"<!-- {} starts -->.*<!-- {} ends -->".format(
+                WOTD_TAG, WOTD_TAG), re.DOTALL,)
+
+        wrapped_new_wotd = WORD_FRAMEWORK.format(new_wotd)
+        new_wotd_text = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(
+            WOTD_TAG, wrapped_new_wotd, WOTD_TAG)
+
+        self.new_readme = regex_pattern.sub(new_wotd_text, self.readme_data)
+
+
+    def save_readme(self):
+        with open(self.filepath, 'w') as write_file:
+            write_file.write(self.new_readme)
+
 
 def fetch_word_of_the_day():
     rss_feed = feedparser.parse(URL)
@@ -19,28 +52,12 @@ def fetch_word_of_the_day():
     return word_of_the_day
 
 
-def get_readme_data():
-    readme = os.path.join(ROOT, 'README.md')
-
-    with open(readme) as read_file:
-        readme_data = read_file.read()
-
-    return readme_data
-
-
-def replace_word_of_the_day():
-    pass
-
-
-def save_readme_file():
-    pass
-
-
 def main():
-    readme_data = get_readme_data()
-    print(readme_data)
     word_of_the_day = fetch_word_of_the_day()
-    print(word_of_the_day)
+  
+    readme = ReadMe()
+    readme.replace_word_of_the_day(new_wotd=word_of_the_day)
+    readme.save_readme()
 
 
 if __name__ == '__main__':
